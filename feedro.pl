b9581a2795672066af283f81b407f82317763056
Kang-min Liu <gugod@gugod.org>
2019-03-12 08:56:16 +0900

    storage

--- feedro.pl
+++ feedro.pl
@@ -4,15 +4,47 @@ use strict;
 use warnings;
 use Mojolicious::Lite;
 use JSON::Feed;
+use Path::Tiny qw< path >;
+
+use constant FEEDRO_STORAGE_DIR => $ENV{FEEDRO_STORAGE_DIR} // '';
 
 # Storage
 my %feeds;
+
+sub save_feeds {
+    return unless FEEDRO_STORAGE_DIR;
+
+    for my $id (keys %feeds) {
+        my $str = $feeds{$id}->to_string;
+        path(FEEDRO_STORAGE_DIR, "${id}.json")->spew_utf8($str);
+    }
+
+    return;
+}
+
+sub load_feeds {
+    return unless FEEDRO_STORAGE_DIR;
+    path(FEEDRO_STORAGE_DIR)->mkpath();
+    path(FEEDRO_STORAGE_DIR)->visit(
+        sub {
+            my ($path) = @_;
+            return unless $path =~ /\.json$/;
+            my $id = $path->basename('.json');
+            my $content = $path->slurp_utf8();
+            $feeds{$id} = JSON::Feed->parse( \$content );
+        },
+        { recurse => 0, follow_symlinks => 0 },
+    );
+}
+
+# main
 
 put '/feed/:identifier' => sub {
     my ($c) = @_;
     my $id = $c->param('identifier');
     my $feed = $c->req->json;
     $feeds{$id} = JSON::Feed->new(%$feed);
+
     $c->render( json => { "ok" => \1 });
 };
 
@@ -23,6 +55,8 @@ post '/feed/:identifier/items' => sub {
 
     my $feed = $feeds{$id};
     $feed->add_item(%$item);
+    save_feeds();
+    
     $c->render( json => { "ok" => \1 });
 };
 
@@ -33,5 +67,5 @@ get '/feed/:identifier' => sub {
     $c->render( json => $feed->feed );
 };
 
-
+load_feeds();
 app->start;
