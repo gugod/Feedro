05b65eebe0c6a331e3850908036311f95ba734aa
Kang-min Liu <gugod@gugod.org>
2019-09-18 23:26:41 +0900

    url. An important parameter.

---
+++ feedro-post.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+use v5.18;
+use strict;
+use warnings;
+use Mojo::UserAgent;
+use Getopt::Long qw< GetOptions >;
+use JSON qw< encode_json >;
+use Data::UUID;
+use Data::Dumper qw< Dumper >;
+
+my %opts;
+GetOptions(
+    \%opts,
+    "title=s",
+    "content_text=s",
+    "url=s",
+    "token=s"
+);
+
+my $feed_url = $ARGV[0] or die "A feed URL is required.";
+die "`title` is required.\n" unless $opts{title};
+die "`token` is required.\n" unless $opts{token};
+
+my %item;
+
+$item{id} = Data::UUID->new->create_str;
+
+for my $k (qw<title url content_text>) {
+    next unless exists $opts{$k};
+    $item{$k} = $opts{$k};
+    utf8::decode( $item{$k} );
+}
+
+$feed_url =~ s{\.json}{/items};
+
+my $ua = Mojo::UserAgent->new;
+my $tx = $ua->post(
+    $feed_url,
+    { Authentication => "Bearer $opts{token}" },
+    json => \%item,
+);
+my $res = $tx->result;
+if ($res->is_error) {
+    say "Error: " . $res->message;
+} elsif ($res->is_success) {
+    say 'Success';
+} else {
+    say "Not sure what happened... Response:";
+    say $res->code;
+    say $res->body;
+}
+
+__END__
+
+feedro-post.pl https://example.com/feed/links.json --token ooxxooxxooxxooxx --url 'https://example.com/item/1' --title 'A new item is here' --content_text 'Some text content here.'
