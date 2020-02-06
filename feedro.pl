2a0ee8a17af950b56ad9ab2a5116d462244c951b
Kang-min Liu <gugod@gugod.org>
2020-02-06 23:04:25 +0900

    limit to 100 items per feed.

--- feedro.pl
+++ feedro.pl
@@ -160,8 +160,8 @@ sub append_item {
     $item->{date_published} //= Time::Moment->now->strftime('%Y-%m-%dT%H:%M:%S%f%Z');
 
     $feed->add_item(%$item);
-    if ( @{ $feed->feed->{items} } > 25 ) {
-        shift @{ $feed->feed->{items} };
+    if ( @{ $feed->feed->{items} } > 100 ) {
+        splice @{ $feed->feed->{items} }, 100;
     }
 
     save_feed($feed_id, $feed);
